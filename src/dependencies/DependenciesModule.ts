f8113e4cf799adede97562b9069d7f7787b34b56
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-11-15 14:43:17 +0300

    Lib update

--- src/dependencies/DependenciesModule.ts
+++ src/dependencies/DependenciesModule.ts
@@ -1,7 +1,6 @@
 import { GenericObjectsByTypeModel, serviceLocatorAdd } from "@flashist/flibs";
 
 import { BaseAppModule } from "../base/modules/BaseAppModule";
-import { getItem, getItemsForType } from "./DependenciesGenericObjectsShortucts";
 
 export class DependenciesModule extends BaseAppModule {
 
