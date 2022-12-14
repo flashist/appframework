bcca06c7fde831cb98858e8447a9520a20dd0e67
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-12-14 15:37:39 +0300

    Update

--- src/size/tools/ViewWithSizeAreaTools.ts
+++ src/size/tools/ViewWithSizeAreaTools.ts
@@ -1,16 +1,16 @@
 import { Rectangle } from "pixi.js";
 import { IViewWithSizeArea } from "../views/IViewWithSizeArea";
 
-export class ViewWithSizeTools {
-    static getSizeRect(view: IViewWithSizeArea, result?: Rectangle): Rectangle {
+export class ViewWithSizeAreaTools {
+    static getSizeAreaBounds(view: IViewWithSizeArea, result?: Rectangle): Rectangle {
 
         if (!result) {
             result = new Rectangle();
         }
         
         if (view.sizeArea) {
-            result.x = view.sizeArea.x * view.scale.x;
-            result.y = view.sizeArea.y * view.scale.y;
+            result.x = view.x + view.sizeArea.x * view.scale.x;
+            result.y = view.y + view.sizeArea.y * view.scale.y;
             //
             result.width = view.sizeArea.width * view.scale.x;
             result.height = view.sizeArea.height * view.scale.y;
