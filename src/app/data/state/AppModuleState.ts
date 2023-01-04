5180614ccafcea4160b08cef3d7e944a38518939
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-01-04 12:19:17 +0300

    Update

--- src/app/data/state/AppModuleState.ts
+++ src/app/data/state/AppModuleState.ts
@@ -18,7 +18,7 @@ export const AppModuleInitialState = {
                 x: 0,
                 y: 0,
                 width: 1920,
-                height: 1080
+                height: 1280
             } as Rectangle
         }
     }
