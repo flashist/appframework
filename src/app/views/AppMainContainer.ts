91d12394a7119a5e67170ad3c1a0c7b5807c2f2e
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-01-05 18:45:28 +0300

    Update

--- src/app/views/AppMainContainer.ts
+++ src/app/views/AppMainContainer.ts
@@ -1,13 +1,24 @@
 import { PagesView } from "../../pages/views/PagesView";
 import { ResizableContainer } from "../../display/views/resize/ResizableContainer";
 import { BaseAppView } from "../../base/views/BaseAppView";
+import { DisplayObjectContainer, Graphics } from '@flashist/flibs';
 
 export class AppMainContainer extends BaseAppView {
+
+    protected bg: DisplayObjectContainer;
+    protected bgColor: number;
+    protected bgAlpha: number;
 
     protected pagesView: PagesView;
 
     protected construction(...args): void {
+        this.bgColor = 0xFFFFFF;
+        this.bgAlpha = 0;
+
         super.construction(...args);
+
+        this.bg = this.createBg();
+        this.addChild(this.bg);
 
         this.pagesView = new PagesView();
         this.addChild(this.pagesView);
@@ -16,9 +27,20 @@ export class AppMainContainer extends BaseAppView {
     protected arrange(): void {
         super.arrange();
 
+        // Make the bg a bit bigger to cover the whole screen 100%
+        this.bg.width = this.resizeSize.x + 2;
+        this.bg.height = this.resizeSize.y + 2;
+
         this.pagesView.resize(
             this.resizeSize.x,
             this.resizeSize.y
         );
+    }
+
+    protected createBg(): DisplayObjectContainer {
+        let result: Graphics = new Graphics();
+        result.beginFill(this.bgColor, this.bgAlpha);
+
+        return result;
     }
 }
