ea2d9be016164e0d38b8591137300010ed77def8
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-01-12 15:14:34 +0300

    Update

--- src/base/commands/BaseAppCommand.ts
+++ src/base/commands/BaseAppCommand.ts
@@ -1,12 +1,12 @@
-import {Command} from "@flashist/fcore";
-import {GlobalEventDispatcher} from "../../globaleventdispatcher/dispatcher/GlobalEventDispatcher";
-import {getInstance} from "@flashist/flibs";
+import { Command } from "@flashist/fcore";
+import { GlobalEventDispatcher } from "../../globaleventdispatcher/dispatcher/GlobalEventDispatcher";
+import { getInstance } from "@flashist/flibs";
 
 export abstract class BaseAppCommand<ResolveType = any> extends Command<ResolveType> {
 
     protected globalDispatcher: GlobalEventDispatcher;
 
-    protected construction(...args): void {
+    construction(...args): void {
         super.construction(...args);
 
         this.globalDispatcher = getInstance(GlobalEventDispatcher);
