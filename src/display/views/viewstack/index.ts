46fee41b86c3563df4345712afae2c12f5c7c5f6
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2025-04-15 12:08:22 +0300

    Update (removing an unused files)

--- src/display/views/viewstack/index.ts
+++ src/display/views/viewstack/index.ts
@@ -2,4 +2,3 @@ export * from './IViewStackItem';
 export * from './ViewLazyCreationServiceLocatorStack';
 export * from './ViewLazyCreationStack';
 export * from './ViewStack';
-export * from './ViewStackLazyCreation';
