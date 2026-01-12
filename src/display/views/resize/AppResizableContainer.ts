681c97cee8c9b83a28d3ac6b797026f2f7ecef0e
Mark Dolbyrev <ruflashist@gmail.com>
2026-01-12 12:51:05 +0300

    Update

--- src/display/views/resize/AppResizableContainer.ts
+++ src/display/views/resize/AppResizableContainer.ts
@@ -1,36 +1,49 @@
-import { Point } from "@flashist/flibs";
+import { Graphics, DisplayTools, Point } from "@flashist/flibs";
 import { AppContainer } from "../AppContainer";
 import { IResizable } from "./IResizable";
 
 export class AppResizableContainer<DataType extends any = any> extends AppContainer<DataType> implements IResizable {
 
     private _resizeSize: Point;
-    private _maxSize: Point;
-    private _minSize: Point;
+
+    private debugResizeSizeView: Graphics;
+    private _debugResizeActive: boolean;
+    public get debugResizeActive(): boolean {
+        return this._debugResizeActive;
+    }
+    public set debugResizeActive(value: boolean) {
+        if (value === this.debugResizeActive) {
+            return;
+        }
+
+        this._debugResizeActive = value;
+
+        if (this._debugResizeActive) {
+            if (!this.debugResizeSizeView) {
+                this.debugResizeSizeView = new Graphics();
+                this.addChildAt(this.debugResizeSizeView, 0);
+                //
+                this.debugResizeSizeView.rect(0, 0, 10, 10);
+                this.debugResizeSizeView.fill({ color: 0xFF0000, alpha: 0.25 });
+            }
+
+        } else {
+            if (this.debugResizeSizeView) {
+                DisplayTools.childRemoveItselfFromParent(this.debugResizeSizeView);
+                this.debugResizeSizeView = null;
+            }
+        }
+
+        this.arrange();
+    }
 
     protected construction(...args): void {
         super.construction(...args);
 
-        this._minSize = new Point();
-        this._maxSize = new Point();
         this._resizeSize = new Point();
     }
 
     public resize(width: number, height: number): void {
-        if (this._minSize.x) {
-            width = Math.max(width, this._minSize.x)
-        }
-        if (this._maxSize.x) {
-            width = Math.min(width, this._maxSize.x)
-        }
-
-        if (this._minSize.y) {
-            height = Math.max(width, this._minSize.y)
-        }
-        if (this._maxSize.y) {
-            height = Math.min(height, this._maxSize.y)
-        }
-
         this.resizeSize.x = width;
         this.resizeSize.y = height;
 
@@ -39,40 +52,17 @@ export class AppResizableContainer<DataType extends any = any> extends AppContai
         }
     }
 
-    get minSize(): Point {
-        return this._minSize;
-    }
-    set minSize(value: Point) {
-        if (!value) {
-            return;
-        }
-        if (value.x === this._minSize.x && value.y === this._minSize.y) {
-            return;
-        }
-
-        this._minSize = value.clone();
-
-        this.resize(this.resizeSize.x, this.resizeSize.y);
-    }
-
-    get maxSize(): Point {
-        return this._maxSize;
-    }
-    set maxSize(value: Point) {
-        if (!value) {
-            return;
-        }
-        if (value.x === this._maxSize.x && value.y === this._maxSize.y) {
-            return;
-        }
-
-        this._maxSize = value.clone();
-
-        this.resize(this.resizeSize.x, this.resizeSize.y);
-    }
-
     public get resizeSize(): Point {
         return this._resizeSize;
+    }
+
+    protected arrange(): void {
+        super.arrange();
+
+        if (this.debugResizeActive) {
+            this.debugResizeSizeView.width = this.resizeSize.x;
+            this.debugResizeSizeView.height = this.resizeSize.y;
+        }
     }
     // public set resizeSize(value: Point) {
     //     throw new Error("Resize size should be changed through the resize method!");
