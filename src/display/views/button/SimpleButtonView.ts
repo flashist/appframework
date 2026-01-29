50279550b92ee83fec303fbc7d8cca9d6854dcff
Mark Dolbyrev <ruflashist@gmail.com>
2026-01-29 14:28:26 +0300

    Update

--- src/display/views/button/SimpleButtonView.ts
+++ src/display/views/button/SimpleButtonView.ts
@@ -411,8 +411,8 @@ export class SimpleButtonView<DataType extends object = object> extends AppResiz
         //
         // this.bg.rect(0, 0, this.bgCalculatedWidth, this.bgCalculatedHeight);
         this.bg.roundRect(0, 0, this.bgCalculatedWidth, this.bgCalculatedHeight, tempBgConfig.bgCornerRadius);
-        this.bg.stroke({ color: tempBgConfig.bgLineColor, alpha: tempBgConfig.bgLineAlpha, width: tempBgConfig.bgLineWidth, alignment: 1 })
         this.bg.fill({ color: tempBgConfig.bgColor, alpha: tempBgConfig.bgAlpha });
+        this.bg.stroke({ color: tempBgConfig.bgLineColor, alpha: tempBgConfig.bgLineAlpha, width: tempBgConfig.bgLineWidth, alignment: 1 })
     }
 
     // OLD
